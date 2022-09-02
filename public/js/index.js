f5a5ff3262d40c4bb6c89f485eb9fe9b94a41973
Adam Awartani <adamawartani321@gmail.com>
2022-09-01 21:30:22 -0400

    load service worker

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,17 @@
+// Ensure that the browser supports the service worker API
+if (navigator.serviceWorker) {
+  // Start registration process on every page load
+  window.addEventListener('load', () => {
+    navigator.serviceWorker
+      // The register function takes as argument
+      // the file path to the worker's file
+      .register('/service-worker.js')
+      // Gives us registration object
+      .then(reg => console.log('Service Worker Registered'))
+      .catch(swErr => console.log(
+        `Service Worker Installation Error: ${swErr}}`));
+  });
+}
 let transactions = [];
 let myChart;
 
